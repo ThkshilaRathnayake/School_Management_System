7c8b9ec7069e4da90a005a92a491a28b13b1a5b2
ThakshilaRathnayake <thakshilarathnayake97@gmail.com>
2024-05-27 09:05:30 +0530

    Reapply "Registration Complete"
    This reverts commit 8813d1177c236e936cc700822124c868609686b8.

---
+++ new_school_management_system/database/migrations/2024_05_22_071323_create_teachers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullName');
+            $table->date('DOB');
+            $table->string('gender');
+            $table->string('NICnumber')->unique();
+            $table->string('employeeID')->unique();
+            $table->string('experience');
+            $table->string('qualifications');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
