929ff093753859d890c09aac42870856d7467bc9
ThakshilaRathnayake <thakshilarathnayake97@gmail.com>
2024-06-27 15:11:01 +0530

    seeder

--- new_school_management_system/database/seeders/DatabaseSeeder.php
+++ new_school_management_system/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RegisterSeeder::class,
+            AdminSeeder::class,
+            TeacherSeeder::class,
+            StudentSeeder::class,
         ]);
+
     }
 }
